1863fad3723de50cb8ca30ec6ebd9d585a6c7fff
BDesselle <desselle.brandon2@gmail.com>
2019-05-18 04:27:11 -0500

    MVP

--- data.sql
+++ data.sql
@@ -11,11 +11,11 @@ INSERT INTO author (authorid, fname, lname) VALUES (4, 'Wells', 'Teague');
 INSERT INTO author (authorid, fname, lname) VALUES (5, 'George', 'Gallinger');
 INSERT INTO author (authorid, fname, lname) VALUES (6, 'Ian', 'Stewart');
 
-INSERT INTO book (bookid, title, ISBN, copy, sectionid) VALUES (1, 'Flatterland', '9780738206752', 2001, 1);
-INSERT INTO book (bookid, title, ISBN, copy, sectionid) VALUES (2, 'Digital Fortess', '9788489367012', 2007, 1);
-INSERT INTO book (bookid, title, ISBN, copy, sectionid) VALUES (3, 'The Da Vinci Code', '9780307474278', 2009, 1);
-INSERT INTO book (bookid, title, ISBN, copy, sectionid) VALUES (4, 'Essentials of Finance', '1314241651234', NULL, 4);
-INSERT INTO book (bookid, title, ISBN, copy, sectionid) VALUES (5, 'Calling Texas Home', '1885171382134', 2000, 3);
+INSERT INTO books (bookid, created_by, created_date, last_modified_by, last_modified_date, isbn, bookname, copy) VALUES (1, 'SYSTEM', 2019-05-18 05:04:24.084000, 'SYSTEM', 2019-05-18 05:04:24.084000, '9780738206752', 'Flatterland', 2001);
+INSERT INTO books (bookid, bookname, copy, isbn) VALUES (2, 'Digital Fortess', 2007, '9788489367012');
+INSERT INTO books (bookid, bookname, copy, isbn) VALUES (3, 'The Da Vinci Code', 2009, '9780307474278');
+INSERT INTO books (bookid, bookname, copy, isbn) VALUES (4, 'Essentials of Finance', NULL, '1314241651234');
+INSERT INTO books (bookid, bookname, copy, isbn) VALUES (5, 'Calling Texas Home', 2000, '1885171382134');
 
 INSERT INTO wrote (bookid, authorid) VALUES (1, 6);
 INSERT INTO wrote (bookid, authorid) VALUES (2, 2);
